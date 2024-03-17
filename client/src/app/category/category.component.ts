c2563d1ffac8ff62b05f579762f47481a27980eb
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-03-17 09:18:25 +0200

    Add category page

--- client/src/app/category/category.component.ts
+++ client/src/app/category/category.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent {
-
+  genres = [
+    'action',
+    'adventure',
+    'animation',
+    'biography',
+    'comedy',
+    'documentary',
+    'drama',
+    'family',
+    'fantasy',
+    'history',
+    'horror',
+    'music',
+    'musical',
+    'mystery',
+    'romance',
+    'sci-fi',
+    'sport',
+    'thriller',
+    'war',
+    'western',
+  ];
 }
